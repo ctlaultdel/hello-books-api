a9531f14599fa3e010218bedd3486825d5e05652
larissa <larza.ault@gmail.com>
2022-12-13 21:42:17 -0800

    Registered books_bp Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-# start-up logic for Flask API
 from flask import Flask
 
+# start-up logic for Flask API
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    # tell app to use the following endpoints for its routing
-    from .routes import hello_world_bp
+    # register the hello_world_bp Blueprint
+    # i.e. tell app to use the following endpoints for its routing
+    from .routes import hello_world_bp, books_bp
     app.register_blueprint(hello_world_bp)
+    app.register_blueprint(books_bp)
 
     return app
