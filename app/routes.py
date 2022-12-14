a445b47ced20a7b0b89ff9ad183cf50bd2c7e20b
larissa <larza.ault@gmail.com>
2022-12-13 21:42:51 -0800

    Added Books class, 3 book instances, and /books endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 # Blueprint groups related routes (endpoints)
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-# store Blueprint instance 
+# store Blueprint instances
 # name convention: name related to data being served or functionality provided
 hello_world_bp = Blueprint("hello_world", __name__)
 
+# ~~~~~~ Hello world endpoints ~~~~~~
 # define endpoint for request and list of HTTP methods request can have
 @hello_world_bp.route("/hello_word", methods=["GET"])
 def hello_world():
@@ -30,3 +31,36 @@ def broken_endpoint():
     new_hobby = "Surfing"
     response_body["hobbies"].append(new_hobby)
     return response_body
+
+
+# ~~~~~~ Book Class ~~~~~~
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Lollipops in the Rain", "A kids book about eating lollipops in the rain"),
+    Book(2, "Dragons at sunrise", "A fantasy book about a child with a dragon friend"),
+    Book(3, "Mai Tai Anytime", "An adult fiction book about a man who loves drinking Mai Tai's")
+    ]
+
+
+# url_prefix routes this blueprint to all routes that start with /books
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+# ~~~~~~ books endpoint ~~~~~~
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    # initialize list
+    books_response = []
+    # format book attributes for each book
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    # return converted json
+    return jsonify(books_response)
