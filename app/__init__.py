75754d0945d76e02df804377c311464a41437c72
larissa <larza.ault@gmail.com>
2022-12-14 23:26:20 -0800

    Corrected module import paths for refactored routes modules

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    
+    from app.routes.hello_world import hello_world_bp
+    from app.routes.books import books_bp
     # register the hello_world_bp Blueprint
     # i.e. tell app to use the following endpoints for its routing
-    from .routes import hello_world_bp, books_bp
     app.register_blueprint(hello_world_bp)
     app.register_blueprint(books_bp)
 
