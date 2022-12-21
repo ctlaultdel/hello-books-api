5241871be3c99b8a2709068a8c767c3f352582aa
larissa <larza.ault@gmail.com>
2022-12-20 19:44:00 -0800

    Refactored GET and POST functions for /books endpoint --> create_new_book() and read_all_books

--- app/routes/books.py
+++ app/routes/books.py
@@ -2,14 +2,14 @@ from app import db
 from app.models.book import Book
 from flask import Blueprint, jsonify, make_response, request
 
-books_bp = Blueprint('books', __name__, url_prefix='/books')
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route('', methods=['POST'])
-def handle_books():
+@books_bp.route('', methods=["POST"])
+def create_new_book():
     request_body = request.get_json()
     if "title" not in request_body or "description" not in request_body:
         return make_response(
-            'Invalid Request: Book title and description required',
+            "Invalid Request: Book title and description required",
             400
         )
     new_book = Book(
@@ -19,5 +19,18 @@ def handle_books():
     db.session.add(new_book)
     db.session.commit()
     return make_response(
-        f'{new_book.title} Successfully Created', 201
+        f"{new_book.title} Successfully Created", 201
     )
+
+@books_bp.route('', methods=["GET"])
+def read_all_books():
+    # query all reports in db using Book instance
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description,
+        })
+    return jsonify(books_response), 200
