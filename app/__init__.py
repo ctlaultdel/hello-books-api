9a2351752a82a5720a41b055bbb1db58bbeb1a06
larissa <larza.ault@gmail.com>
2022-12-31 12:14:01 -0800

    Created secure variables for connection strings to either testing or development db

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-# connection string for finding db
-c_str = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+from dotenv import load_dotenv
+import os
 
 # create connection point to db as db obj
 db = SQLAlchemy()
 # compare_type allows you to detect changes in dtype in migrations
 migrate = Migrate(compare_type=True)
+# load .env variables
+load_dotenv()
 
 def create_app(test_config=None):
     """
     Start-up logic for Flask API
+    :params: 
+    - test_config (bool): flag for test/development mode
     """
     app = Flask(__name__)
     # Turn off SQLAlchemy event sys to save sys resources
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # connect to db using connection string
-    app.config['SQLALCHEMY_DATABASE_URI'] = c_str
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # connect to db
+    if not test_config:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DB_URI")
+    else:
+        # turn testing mode on
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DB_URI")
     # connect db & migrate objs to Flask app
     db.init_app(app)
     migrate.init_app(app, db)
