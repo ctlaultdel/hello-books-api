185814eb26773f32ca4d153f358799ed492923d8
larissa <larza.ault@gmail.com>
2023-01-01 17:59:53 -0800

    Created and passed unit test for testing get book by book id

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,3 +7,17 @@ def test_get_all_books_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body=response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    # is there a way to call ocean book from two_saved_books fixture & reformat in json rather than hardcode??
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
