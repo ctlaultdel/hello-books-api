1d88cebe3fabb3d1bc93d55cd3b1dcbc7a076820
larissa <larza.ault@gmail.com>
2023-01-01 18:31:24 -0800

    Used jsonify() method for returns in different http methods to circumvent error in unit tests

--- app/routes/books.py
+++ app/routes/books.py
@@ -19,8 +19,9 @@ def create_new_book():
     )
     db.session.add(new_book)
     db.session.commit()
-    return make_response(
-        f"{new_book.title} Successfully Created", 201
+    return make_response(jsonify(
+        f"{new_book.title} Successfully Created"),
+        201
     )
 
 @books_bp.route("", methods=["GET"])
@@ -58,15 +59,15 @@ def update_one_book(book_id):
     book.title = request_body["title"]
     book.description = request_body["description"]
     db.session.commit()
-    return make_response(
+    return make_response(jsonify(
         f"Book #{book.id} Successfully Updated"
-    )
+    ))
 
 @books_bp.route("/<book_id>", methods=["DELETE"])
 def delete_one_book(book_id):
     book = validate_book(book_id)
     db.session.delete(book)
     db.session.commit()
-    return make_response(
+    return make_response(jsonify(
         f"Book #{book.id} Successfully Deleted"
-    )
+    ))
